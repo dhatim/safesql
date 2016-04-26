6280af8ade371a2f87e71af5dc658657c8786643
Mathieu <mligocki@dhatim.com>
2016-04-26 17:45:29 +0200

    Prevent deploying for tagged

--- .deploy.sh
+++ .deploy.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [[ ${TRAVIS_BRANCH} != 'master' ]]; then
-    echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
-    exit
-fi
-
 if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
     echo "Skipping deployment for pull request"
     exit
@@ -15,7 +10,11 @@ fi
 if [[ -n ${TRAVIS_TAG} ]]; then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings maven_deploy_settings.xml org.codehaus.mojo:versions-maven-plugin:2.2:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
+else 
+		if [[ ${TRAVIS_BRANCH} != 'master' ]]; then
+		    echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
+		    exit
+		fi
 fi
-
 
 mvn -B deploy --settings maven_deploy_settings.xml -Dmaven.test.skip=true -Dfindbugs.skip=true
