fe98c7cb3604d263c8834d6b3c53ddbfc017dfb6
Mathieu <mligocki@dhatim.com>
2016-04-26 18:45:03 +0200

    Add release instead of deploy

--- .deploy.sh
+++ .deploy.sh
@@ -10,11 +10,12 @@ fi
 if [[ -n ${TRAVIS_TAG} ]]; then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings maven_deploy_settings.xml org.codehaus.mojo:versions-maven-plugin:2.2:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
+    echo "release deploy"
+    mvn -B clean deploy release --settings maven_deploy_settings.xml -Dfindbugs.skip=true -Djacoco.skip=true -P release
 else 
 		if [[ ${TRAVIS_BRANCH} != 'master' ]]; then
 		    echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
 		    exit
 		fi
+		mvn -B deploy --settings maven_deploy_settings.xml -DskipTests=true -Dfindbugs.skip=true -Djacoco.skip=true
 fi
-
-mvn -B verify deploy --settings maven_deploy_settings.xml -DskipTests=true -Dfindbugs.skip=true -Djacoco.skip=true
