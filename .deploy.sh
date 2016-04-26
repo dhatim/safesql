dea857fcadfc72162b69b584ebeb75e4604e1885
Mathieu <mligocki@dhatim.com>
2016-04-26 17:17:08 +0200

    Add debug flag to deploy script

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ -n ${TRAVIS_TAG} ]]; then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings maven_deploy_settings.xml org.codehaus.mojo:versions-maven-plugin:2.2:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
