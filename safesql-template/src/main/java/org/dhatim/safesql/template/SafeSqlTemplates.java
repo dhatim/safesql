dde3d5ba58c8be9671e1ad61c9786e0ac87298aa
Mathieu <mligocki@dhatim.com>
2016-05-13 15:14:02 +0200

    Add template samples

--- safesql-template/src/main/java/org/dhatim/safesql/template/SafeSqlTemplates.java
+++ safesql-template/src/main/java/org/dhatim/safesql/template/SafeSqlTemplates.java
@@ -19,3 +19,28 @@ public interface SafeSqlTemplates {
     }
 
 }
+
+/*
+
+@Template("SELECT * FROM :table WHERE name = :name")
+SafeSql query(@Identifier String table, String name);
+
+@Template("SELECT * FROM :schemaTable WHERE name = :name")
+SafeSql query(@DottedSchemaTable String schemaTable, String name);
+
+@Template("WITH :cte AS cte, SELECT * FROM :table t INNER JOIN cte c ON c.id = t.cte_id")
+SafeSql query(SafeSql cte, @Identifier String table, String name);
+
+@Template("SELECT * FROM table WHERE name IN (:names)")
+SafeSql query(List<String> names);
+
+@Template("SELECT * FROM table WHERE name = :name")
+SafeSqlizable query(String name);
+
+@Template("INSERT INTO :errorTable (\"DECLARATION\", \"FILE\", \"CONTROL\", \"REVISION\", line, \"rowId\") SELECT DISTINCT \"DECLARATION\", :fileId, :controlId, :revId, \"firstLine\", id FROM (:sql) errors")
+@Var(name="errorTable", value="error_{fileId}", type=@Identifier)
+SafeSql insert(UUID fileId, int controlId, int revId, SafeSql sql);
+
+
+
+*/
