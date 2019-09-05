67e9893112c8b69cfc4e848dd10921bf4e2f44d4
Olivier Chédru <ochedru@dhatim.com>
2019-09-05 11:05:29 +0200

    Escape properly array literals
    Also, soft coding for readability

--- safesql/src/main/java/org/dhatim/safesql/ArraySupport.java
+++ safesql/src/main/java/org/dhatim/safesql/ArraySupport.java
@@ -6,12 +6,25 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
-import java.util.UUID;
+import java.util.*;
 
 final class ArraySupport {
 
-    private static final char VALUE_QUOTE = '"';
     private static final char[] HEX_CODE = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    private static final Map<Character, String> ESCAPES;
+
+    static {
+        Map<Character, String> m = new HashMap<>();
+        m.put('\'', "''");
+        m.put('"', "\\\"");
+        m.put('\\', "\\\\");
+        m.put('\b', "\\b");
+        m.put('\f', "\\f");
+        m.put('\n', "\\n");
+        m.put('\r', "\\r");
+        m.put('\t', "\\t");
+        ESCAPES = Collections.unmodifiableMap(m);
+    }
 
     private ArraySupport() {
     }
@@ -56,28 +69,52 @@ final class ArraySupport {
         } else if (obj instanceof Number) {
             sb.append(obj);
         } else if (obj instanceof Timestamp) {
-            sb.append(VALUE_QUOTE).append(SafeSqlUtils.TIMESTAMP_FORMAT_WITH_TZ.format((Timestamp) obj)).append(VALUE_QUOTE);
+            synchronized (SafeSqlUtils.TIMESTAMP_FORMAT_WITH_TZ) {
+                sb.append('"').append(SafeSqlUtils.TIMESTAMP_FORMAT_WITH_TZ.format((Timestamp) obj)).append('"');
+            }
         } else if (obj instanceof Time) {
-            sb.append(VALUE_QUOTE).append(SafeSqlUtils.TIME_FORMAT_WITH_TZ.format((Time) obj)).append(VALUE_QUOTE);
+            synchronized (SafeSqlUtils.TIME_FORMAT_WITH_TZ) {
+                sb.append('"').append(SafeSqlUtils.TIME_FORMAT_WITH_TZ.format((Time) obj)).append('"');
+            }
         } else if (obj instanceof java.sql.Date) {
-            sb.append(VALUE_QUOTE).append(((java.sql.Date) obj).toLocalDate()).append(VALUE_QUOTE);
+            sb.append('"').append(((java.sql.Date) obj).toLocalDate()).append('"');
         } else if (obj instanceof LocalDate) {
-            sb.append(VALUE_QUOTE).append(obj).append(VALUE_QUOTE);
+            sb.append('"').append(obj).append('"');
         } else if (obj instanceof LocalTime) {
-            sb.append(VALUE_QUOTE).append(obj).append(VALUE_QUOTE);
+            sb.append('"').append(obj).append('"');
         } else if (obj instanceof LocalDateTime) {
-            sb.append(VALUE_QUOTE).append(obj).append(VALUE_QUOTE);
+            sb.append('"').append(obj).append('"');
         } else if (obj instanceof OffsetDateTime) {
-            sb.append(VALUE_QUOTE).append(SafeSqlUtils.TIMESTAMP_FORMATTER_WITH_TZ.format((OffsetDateTime) obj)).append(VALUE_QUOTE);
+            sb.append('"').append(SafeSqlUtils.TIMESTAMP_FORMATTER_WITH_TZ.format((OffsetDateTime) obj)).append('"');
         } else if (obj instanceof UUID) {
-            sb.append(VALUE_QUOTE).append(obj).append(VALUE_QUOTE);
+            sb.append('"').append(obj).append('"');
         } else if (obj instanceof byte[]) {
-            sb.append(VALUE_QUOTE).append("\\x");
+            sb.append('"').append("\\x");
             appendHexBytes(sb, (byte[]) obj);
-            sb.append(VALUE_QUOTE);
+            sb.append('"');
         } else {
-            sb.append(VALUE_QUOTE).append(obj).append(VALUE_QUOTE);
+            String s = obj.toString();
+            sb.append('"');
+            boolean escape = false;
+            for (char c : s.toCharArray()) {
+                if (ESCAPES.containsKey(c)) {
+                    escape = true;
+                    break;
+                }
+            }
+            if (escape) {
+                for (char c : s.toCharArray()) {
+                    String cs = ESCAPES.get(c);
+                    if (cs == null) {
+                        sb.append(c);
+                    } else {
+                        sb.append(cs);
+                    }
+                }
+            } else {
+                sb.append(obj);
+            }
+            sb.append('"');
         }
     }
-
 }
